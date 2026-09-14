6abd3af54280fb90aaec13ecf289ff5925beab0f
mizwit <165015112+mizwit@users.noreply.github.com>
2026-09-14 15:07:25 +0530

    Updated DS Bubble Sort

--- Semester I/CS-403 Data Structures/Programs/04_bubble_sort.c
+++ Semester I/CS-403 Data Structures/Programs/04_bubble_sort.c
@@ -10,8 +10,49 @@ Write a C program to sort an array using Bubble Sort:
 5. Print the pass number in which array became sorted.
 */
 
+#include <stdio.h>
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
+    int N = 10;
     int arr[10] = {83, 4, 16, 47, 33, 38, 92, 19, 25, 2};
+    
+    printf("Unsorted Array: ");
+    print_array(arr, N);
+    printf("----------\n");
+    
+    int total_comps = 0, total_swaps = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - 1 - i; j++)
+        {   
+            total_comps++;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                total_swaps++;
+            }
+        }
+        printf("Array after Pass #%d: ", i + 1);  // Print AFTER inner loop
+        print_array(arr, N);
+    }
+    printf("----------\n");
+    
+    printf("Sorted Array: ");
+    print_array(arr, N);
+    printf("----------\n");
+    
+    printf("Total Number of Comparisions: %d\n", total_comps);
+    printf("Total Number of Swaps: %d\n", total_swaps);
+
     return 0;
 }
