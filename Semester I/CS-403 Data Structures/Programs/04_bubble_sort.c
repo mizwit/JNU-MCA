a8c29c7a67fea5a9b0728d53c0c7b5a342c406b5
mizwit <165015112+mizwit@users.noreply.github.com>
2026-09-14 14:28:11 +0530

    Add Bubble Sort Question

---
+++ Semester I/CS-403 Data Structures/Programs/04_bubble_sort.c
@@ -0,0 +1,17 @@
+/*
+Write a C program to sort an array using Bubble Sort:
+1. Sort the array in ascendng order.
+2. Count the total number of comparisions and swaps.
+3. After each pass, print the current array.
+4. Determine and print whatever the original array was.
+    - Already sorted
+    - Nearly sorted
+    - Completely unsorted
+5. Print the pass number in which array became sorted.
+*/
+
+int main()
+{
+    int arr[10] = {83, 4, 16, 47, 33, 38, 92, 19, 25, 2};
+    return 0;
+}
